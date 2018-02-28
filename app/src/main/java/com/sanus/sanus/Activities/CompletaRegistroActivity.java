c8f621dd6565375dd5e75ecfa248dc9e9afa2320
Mirellitha27 <anamireyaj@gmail.com>
2018-02-28 10:51:30 -0600

    completando registro 1

--- app/src/main/java/com/sanus/sanus/Activities/CompletaRegistroActivity.java
+++ app/src/main/java/com/sanus/sanus/Activities/CompletaRegistroActivity.java
@@ -1,16 +1,8 @@
 package com.sanus.sanus.Activities;
 
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
+
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.ContactsContract;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,22 +11,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 import com.sanus.sanus.R;
-
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
