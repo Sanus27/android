8fa5906a55b613bc8500ebc658f1f9ae90f2f283
Mirellitha27 <anamireyaj@gmail.com>
2018-02-28 23:09:57 -0600

    Subiendo imagen en el activity completar registro

--- app/src/main/java/com/sanus/sanus/Activities/CompletaRegistroActivity.java
+++ app/src/main/java/com/sanus/sanus/Activities/CompletaRegistroActivity.java
@@ -1,7 +1,13 @@
 package com.sanus.sanus.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,13 +18,18 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.sanus.sanus.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +41,7 @@ public class CompletaRegistroActivity extends AppCompatActivity {
     Button masculino, femenino, guardar;
     Spinner spinnerEdad;
     private CircleImageView setupImage;
+    private Uri mainImageURI = null;
 
     private StorageReference storageReference;
     private FirebaseAuth auth;
@@ -81,8 +93,25 @@ public class CompletaRegistroActivity extends AppCompatActivity {
             }
         });
 
+        setupImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(ContextCompat.checkSelfPermission(CompletaRegistroActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(CompletaRegistroActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(CompletaRegistroActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    } else {
+                        BringImagePicker();
+                    }
+                } else {
+                    BringImagePicker();
+                }
+            }
+        });
 
-    //obtener edad
+
+
+        //obtener edad
     items = getResources().getStringArray(R.array.Edad);
     final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
@@ -109,14 +138,28 @@ public class CompletaRegistroActivity extends AppCompatActivity {
             String edaD = edadPosition;
             masculino.getText().toString();
             femenino.getText().toString();
+
+            StorageReference image_path = storageReference.child("avatar").child(id);
+            image_path.putFile(mainImageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()){
+                        Uri download_uri = task.getResult().getDownloadUrl();
+                        Toast.makeText(CompletaRegistroActivity.this, "The Image is Uploaded", Toast.LENGTH_LONG).show();
+                    }else {
+                        String error = task.getException().getMessage();
+                        Toast.makeText(CompletaRegistroActivity.this, "Error" + error, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
             final Map<String, String> userMap = new HashMap<>();
             userMap.put("tipo", "Paciente");
             userMap.put("nombre", name);
             userMap.put("apellido", apelli);
             userMap.put("edad", edaD);
             userMap.put("sexo", sex);
-            userMap.put("avatar","avatar.png");
-
+            userMap.put("avatar", String.valueOf(image_path));
 
             mFirestore.collection("usuarios").document(id).set(userMap).addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
@@ -138,6 +181,27 @@ public class CompletaRegistroActivity extends AppCompatActivity {
     public void sexo(int num){
         if(num==1){sex = "Masculino";}
         if(num==2){sex= "Femenino";}
+    }
+    private void BringImagePicker() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1, 1)
+                .start(CompletaRegistroActivity.this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                mainImageURI = result.getUri();
+                setupImage.setImageURI(mainImageURI);
+                //isChanged = true;
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 
 }
